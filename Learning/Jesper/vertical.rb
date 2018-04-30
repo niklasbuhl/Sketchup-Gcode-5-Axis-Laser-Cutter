880e7364b1470c8981f9a4286610fcb78933890f
Kirial <jesperkirial@gmail.com>
2018-04-30 14:56:23 +0200

    test

--- Learning/Jesper/vertical.rb
+++ Learning/Jesper/vertical.rb
@@ -10,7 +10,7 @@ sel = mod.selection # Current selection
 
 sel.clear;
         
-    mod.active_entities.grep(Sketchup::Face).each{|f|sel.add(f)if f.normal.z>-0.71 && f.normal.z<0.71}
+    ent.grep(Sketchup::Face).each{|f|sel.add(f)if f.normal.z>-0.71 && f.normal.z<0.71}
 
 
 
@@ -26,3 +26,28 @@ def Angle face
 end
 
 
+
+# By Jesper Kirial and Niklas Buhl
+
+
+require 'sketchup.rb'
+
+mod = Sketchup.active_model # Open model
+ent = mod.entities # All entities in model
+sel = mod.selection # Current selection
+colored = array.new
+
+module Main
+
+
+mod.active_entities.grep(Sketchup::Face).each{ |f|
+    
+    colored.add(f)if f.normal.z>-0,70710678118654 && f.normal.z<0.70710678118654}
+
+    
+def color (colored)
+     colored.each do |face|
+      face =  face.material = "cyan"
+      face = face.back_material ="cyan"
+end
+end

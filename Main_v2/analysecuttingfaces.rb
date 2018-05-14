259bacc81a0dd09b1e89a64da637cfbc3ff4f14f
Kirial <jesperkirial@gmail.com>
2018-05-14 10:18:01 +0200

    Revert "stribes to cutting face outmost vertices"
    tttt reverts commit adb5acd8b18099a810b1818e7212eb24b1b2b8b6.

--- Main_v2/analysecuttingfaces.rb
+++ Main_v2/analysecuttingfaces.rb
@@ -62,7 +62,11 @@ end
 class CuttingFace
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   attr_accessor :face, :edgeCount, :edges, :vertexCount, :manipulatedVertices, :manipulatedEdges, :verticesXY, :topVertex, :bottomVertex, :outerMostSideVertexA, :outerMostSideVertexB, :xyAngleOffset
+=======
+  attr_accessor :face, :edgeCount, :edges, :vertexCount, :manipulatedVertices, :verticesXY, :topVertex, :bottomVertex, :sideVertexA, :sideVertexB, :xyAngleOffset
+>>>>>>> parent of adb5acd... stribes to cutting face outmost vertices
 =======
   attr_accessor :face, :edgeCount, :edges, :vertexCount, :manipulatedVertices, :verticesXY, :topVertex, :bottomVertex, :sideVertexA, :sideVertexB, :xyAngleOffset
 >>>>>>> parent of adb5acd... stribes to cutting face outmost vertices
